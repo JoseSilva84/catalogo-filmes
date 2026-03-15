e313137822aac47a0dbe3fc2ad46ac938f32714a
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-03-15 16:33:06 -0300

    Melhorias no front

--- app-primefilme/src/pages/Filme/index.jsx
+++ app-primefilme/src/pages/Filme/index.jsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+import './filme.css';
+
 const Filme = () => {
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: 'deac86272a92449f6c91e3fc36684014',
+                    language: 'pt-BR',
+                }
+            })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log("Filme não foi encontrado");
+            })
+        }
+        
+        loadFilme();
+    }, [id]);
+
+    if(loading){
+        return (
+            <div className="filme-info">
+                <h1>Carregando detalhes do filme...</h1>
+            </div>
+        )
+    }
+
     return ( 
-        <>
-            <h1>BEM-VINDO A PÁGINA DO FILME</h1>
-        </>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://tmdb.org/t/p/original${filme.backdrop_path}`} alt={filme.title} />
+            
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Nota: {filme.vote_average.toFixed(1)} /10</strong>
+            
+            <div className="area-button">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
+        </div>
      );
 }
  
