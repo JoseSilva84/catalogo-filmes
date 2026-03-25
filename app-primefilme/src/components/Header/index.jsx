5f5c85a1b9a4878593cb3d7b1ed195282f0a9ff2
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-03-24 22:41:05 -0300

    feat: Adição da responsividade

--- app-primefilme/src/components/Header/index.jsx
+++ app-primefilme/src/components/Header/index.jsx
@@ -1,20 +1,42 @@
 import { Link } from 'react-router-dom';
 import './header.css';
+import { useState } from 'react';
 
 import Input from './input';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
       <div className="header-inner">
         <Link className="logo" to="/">
           Catálogo Prime <span className="dot" />
         </Link>
-        <nav>
-          <Link className="btn-nav" to="/">Início</Link>
-          <Link className="btn-nav btn-primary" to="/favoritos">♥ Meus Filmes</Link>
+
+        {/* Desktop Menu */}
+        <nav className="desktop-menu">
           <Input />
+          <Link className="btn-nav btn-primary" to="/favoritos">♥ Meus Filmes</Link>
         </nav>
+
+        {/* Mobile Menu */}
+        <div className="mobile-menu">
+          <div className={`menu-icon ${menuOpen ? 'close' : ''}`} onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+          <nav className={`menu-content ${menuOpen ? 'menu-open' : ''}`}>
+            <Input />
+            <Link className="btn-nav btn-primary" to="/favoritos" onClick={() => setMenuOpen(false)}>♥ Meus Filmes</Link>
+            <button className="btn-close-menu" onClick={toggleMenu}>X</button>
+          </nav>
+        </div>
       </div>
     </header>
   );
